7bf5270343d12c16c8e069a6d789899622f927c3
poopedub <pablo.volt@gmail.com>
2021-01-22 09:49:07 +0100

    fix form styles in chromnium browsers

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -18,11 +18,15 @@
     <h2 class="text-uppercase">@field('titulo_destacado')</h2>
   </div>
 
-  <div class="col-12 col-lg-4 text-lowercase d-lg-flex flex-lg-column justify-content-lg-between f-medium">
+  <div class="col-12 col-lg-4 text-lowercase d-lg-flex flex-lg-column justify-content-lg-between f-medium order-2 order-lg-1">
     <div>
+      <a href="@field('enlace_bottom', 'url')" class="">
       @field('texto_destacado')
+      </a>
     </div>
-    <div><span class="f-small">&#9654; </span> <a href="@field('enlace_bottom', 'url')" class="d-inline-block link-linea"> @field('enlace_bottom','title')</a></div>
+    <div class="d-none d-lg-block">
+      <span class="f-small">&#9654; </span> <a href="@field('enlace_bottom', 'url')" class="d-inline-block link-linea"> @field('enlace_bottom','title')</a>
+    </div>
 
   </div>
 
@@ -40,7 +44,7 @@
     <h2 class="text-uppercase">
       {{ __('ACTIVIDADES', 'automaticatheme') }}
     </h2>
-    <div class="f-small"><span>&#9654; </span><a href="@permalink(207)">{{ __('Ver todas', 'automaticatheme') }}</a></div>
+    <div class="f-small d-none d-lg-block"><span>&#9654; </span><a href="@permalink(207)">{{ __('Ver todas', 'automaticatheme') }}</a></div>
 
   </div>
 
@@ -52,10 +56,10 @@
   ])
 
   @posts
-  <div class="col-12 col-lg-6 f-small">
+  <div class="col-12 col-lg-6 f-small mt-3 mt-lg-0">
     <a href="@permalink">
       <img class="img-fluid mb-2" src="@thumbnail('grande', false)" alt="">
-      <div class="d-flex justify-content-between">
+      <div class="d-lg-flex justify-content-between">
         <div>
           <h3 class="entry-title f-small mb-0 link-linea d-inline-block">@title @field('subtitulo')</h3>
           <span class="d-block">@field('tipo_de_actividad')</span>
@@ -80,7 +84,7 @@
   <div class="col-12 col-lg-6">
     <img class="img-fluid" src="@field('foto_talleres', 'url')" alt="@field('foto_talleres', 'alt')">
   </div>
-  <div class="col-12 col-lg-6 f-small pl-2 d-lg-flex flex-lg-column justify-content-lg-between px-4">
+  <div class="col-12 col-lg-6 f-small pl-2 d-lg-flex flex-lg-column justify-content-lg-between px-lg-4">
     @field('texto_talleres')
     <div class="f-small"><span>&#9654; </span><a href="@permalink(141)" class="link-linea">{{ __('Ver talleres', 'automaticatheme') }}</a>
     </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,27 +2,36 @@
   <div class="container-fluid pt-3 padd-head d-flex align-items-end">
     <a class="brand" href="{{ home_url('/') }}">
       @if (!is_single())
-        <h1>{!! get_bloginfo('name', 'display') !!}</h1>
+      <h1>{!! get_bloginfo('name', 'display') !!}</h1>
       @else
       <div class="ml-auto">{!! get_bloginfo('name', 'display') !!}</div>
 
       @endif
     </a>
-    @php
-    do_action('wpml_add_language_selector');
-    @endphp
+    <div class="d-none d-lg-block wpml-lg">
+      @php
+      do_action('wpml_add_language_selector');
+      @endphp
+    </div>
+
 
     <a id="menu" class="btn-menu">@svg('ico-menu', 'ico-menu')</a>
   </div>
 </header>
 <div class="overlay overlay-slidedown">
-<div class="container-fluid padd-head">
-<button type="button" id="closeMenu" class="overlay-close font-3">@svg('close-menu','ico-close')</button>
-<nav class="nav-primary">
-  @if (has_nav_menu('primary_navigation'))
-  {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-  @endif
-</nav>
-</div>
+  <div class="container-fluid padd-head">
+    <button type="button" id="closeMenu" class="overlay-close font-3">@svg('close-menu','ico-close')</button>
+    <nav class="nav-primary">
+      <div class="d-block d-lg-none">{!! get_bloginfo('name', 'display') !!}</div>
+      @if (has_nav_menu('primary_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+      @endif
+    </nav>
+    <div class="d-block d-lg-none wpml-phone">
+      @php
+      do_action('wpml_add_language_selector');
+      @endphp
+    </div>
+  </div>
 
 </div>
